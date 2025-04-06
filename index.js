e8487f121c949abf324d9bccfbc5832ebe6428bb
Ozodbek311 <serior377@gmail.com>
2025-04-06 19:26:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,593 @@
+let translation = {
+    "computer": "kompyuter", // computer
+    "keyboard": "klaviatura", // keyboard
+    "mouse": "sichqoncha", // mouse
+    "screen": "ekran", // screen
+    "phone": "telefon", // phone
+    "internet": "internet", // internet
+    "website": "vebsayt", // website
+    "email": "elektron pochta", // email
+    "password": "parol", // password
+    "username": "foydalanuvchi nomi", // username
+    "software": "dasturiy ta’minot", // software
+    "hardware": "qattiq jihoz", // hardware
+    "cloud": "bulut", // cloud
+    "network": "tarmoq", // network
+    "security": "xavfsizlik", // security
+    "artificial intelligence": "sun’iy intellekt", // artificial intelligence
+    "machine learning": "mashina o‘rganishi", // machine learning
+    "database": "ma’lumotlar bazasi", // database
+    "server": "server", // server
+    "client": "mijoz", // client
+    "frontend": "old qism", // frontend
+    "backend": "orqa qism", // backend
+    "framework": "framework", // framework
+    "library": "kutubxona", // library
+    "open source": "ochiq kodli", // open source
+    "variable": "o‘zgaruvchi", // variable
+    "function": "funksiya", // function
+    "loop": "tsikl", // loop
+    "condition": "shart", // condition
+    "exception": "istisno", // exception
+    "file": "fayl", // file
+    "directory": "katalog", // directory
+    "execute": "bajarish", // execute
+    "compile": "kompilyatsiya qilish", // compile
+    "syntax": "sintaksis", // syntax
+    "error": "xato", // error
+    "performance": "samaradorlik", // performance
+    "optimization": "optimallashtirish", // optimization
+    "deployment": "joylashtirish", // deployment
+    "algorithm": "algoritm", // algorithm
+    "data": "ma’lumot", // data
+    "object": "obyekt", // object
+    "class": "klass", // class
+    "inheritance": "meros olish", // inheritance
+    "polymorphism": "polimorfizm", // polymorphism
+    "encapsulation": "inkapsulyatsiya", // encapsulation
+    "abstraction": "abstraksiya", // abstraction
+    "interface": "interfeys", // interface
+    "module": "modul", // module
+    "package": "paket", // package
+    "repository": "repositoriy", // repository
+    "version control": "versiya nazorati", // version control
+    "commit": "commit", // commit
+    "branch": "shox", // branch
+    "merge": "birlashtirish", // merge
+    "push": "jo‘natish", // push
+    "pull": "olish", // pull
+    "clone": "klonlash", // clone
+    "database": "bazasi", // database
+    "table": "jadval", // table
+    "column": "ustun", // column
+    "row": "qator", // row
+    "query": "so‘rov", // query
+    "insert": "qo‘shish", // insert
+    "update": "yangilash", // update
+    "delete": "o‘chirish", // delete
+    "select": "tanlash", // select
+    "join": "birlashish", // join
+    "index": "indeks", // index
+    "transaction": "transaksiya", // transaction
+    "backup": "zaxira nusxasi", // backup
+    "restore": "tiklash", // restore
+    "encryption": "shifrlash", // encryption
+    "authentication": "autentifikatsiya", // authentication
+    "authorization": "avtorizatsiya", // authorization
+    "session": "sessiya", // session
+    "cookie": "cookie", // cookie
+    "cache": "keshlash", // cache
+    "API": "API", // API
+    "endpoint": "endpoint", // endpoint
+    "request": "so‘rov", // request
+    "response": "javob", // response
+    "protocol": "protokol", // protocol
+    "HTTP": "HTTP", // HTTP
+    "HTTPS": "HTTPS", // HTTPS
+    "TCP": "TCP", // TCP
+    "UDP": "UDP", // UDP
+    "DNS": "DNS", // DNS
+    "IP address": "IP manzil", // IP address
+    "port": "port", // port
+    "router": "router", // router
+    "firewall": "firewall", // firewall
+    "cybersecurity": "kiberxavfsizlik", // cybersecurity
+    "animal": "hayvon", // animal
+    "cat": "mushuk", // cat
+    "dog": "it", // dog
+    "bird": "qush", // bird
+    "fish": "baliq", // fish
+    "horse": "ot", // horse
+    "cow": "sigir", // cow
+    "sheep": "qo‘y", // sheep
+    "goat": "echki", // goat
+    "pig": "cho‘chqa", // pig
+    "chicken": "tovuq", // chicken
+    "duck": "o‘rdak", // duck
+    "rabbit": "quyon", // rabbit
+    "lion": "yo‘lbars", // lion
+    "tiger": "yovvoyi mushuk", // tiger
+    "elephant": "fil", // elephant
+    "giraffe": "jirafa", // giraffe
+    "zebra": "zebra", // zebra
+    "monkey": "maymun", // monkey
+    "bear": "ayiq", // bear
+    "wolf": "bo‘ri", // wolf
+    "fox": "bo‘ri", // fox
+    "squirrel": "echkikucha", // squirrel
+    "mouse": "sichqon", // mouse
+    "rat": "sichqon", // rat
+    "snake": "ilon", // snake
+    "crocodile": "aligator", // crocodile
+    "shark": "reqa", // shark
+    "whale": "balina", // whale
+    "octopus": "karxona", // octopus
+    "jellyfish": "meduza", // jellyfish
+    "coral": "koral", // coral
+    "tree": "daraxt", // tree
+    "flower": "gul", // flower
+    "plant": "o‘simlik", // plant
+    "leaf": "varaq", // leaf
+    "fruit": "meva", // fruit
+    "seed": "urug‘", // seed
+    "soil": "grunt", // soil
+    "root": "ildiz", // root
+    "branch": "shox", // branch
+    "trunk": "bosh", // trunk
+    "bark": "po‘st", // bark
+    "forest": "o‘rmon", // forest
+    "desert": "cho‘l", // desert
+    "mountain": "tog‘", // mountain
+    "valley": "dolina", // valley
+    "river": "daryo", // river
+    "lake": "ko‘l", // lake
+    "ocean": "okean", // ocean
+    "sea": "deniz", // sea
+    "island": "orol", // island
+    "beach": "sohil", // beach
+    "sand": "qum", // sand
+    "stone": "to‘p", // stone
+    "rock": "tog‘ tosh", // rock
+    "cloud": "bulut", // cloud
+    "rain": "yomg‘ir", // rain
+    "snow": "qor", // snow
+    "storm": "bo‘ron", // storm
+    "wind": "shamol", // wind
+    "thunder": "jilg‘a", // thunder
+    "lightning": "yilduz", // lightning
+    "temperature": "harorat", // temperature
+    "humidity": "namlik", // humidity
+    "earthquake": "zamin silkinishi", // earthquake
+    "volcano": "vulkan", // volcano
+    "tornado": "tornado", // tornado
+    "flood": "to‘fon", // flood
+    "tsunami": "tsunami", // tsunami
+    "fire": "olov", // fire
+    "smoke": "tutun", // smoke
+    "explosion": "portlash", // explosion
+    "pollution": "ifloslanish", // pollution
+    "clean": "toza", // clean
+    "dirty": "iflos", // dirty
+    "bright": "yorqin", // bright
+    "dark": "qorong‘i", // dark
+    "hot": "issiqlik", // hot
+    "cold": "sovuq", // cold
+    "warm": "iliq", // warm
+    "cool": "salqin", // cool
+    "wet": "nam", // wet
+    "dry": "quruq", // dry
+    "soft": "yumshoq", // soft
+    "hard": "qattiq", // hard
+    "sharp": "o‘tkir", // sharp
+    "dull": "sayqallashmagan", // dull
+    "fast": "tez", // fast
+    "slow": "sekin", // slow
+    "heavy": "og‘ir", // heavy
+    "light": "yengil", // light
+    "big": "katta", // big
+    "small": "kichik", // small
+    "tall": "baland", // tall
+    "short": "qisqa", // short
+    "wide": "keng", // wide
+    "narrow": "tor", // narrow
+    "long": "uzun", // long
+    "deep": "chuqur", // deep
+    "shallow": "yuzaki", // shallow
+    "round": "dumaloq", // round
+    "square": "kvadrat", // square
+    "triangle": "uchburchak", // triangle
+    "rectangle": "to‘rtburchak", // rectangle
+    "circle": "doira", // circle
+    "color": "rang", // color
+    "red": "qizil", // red
+    "blue": "ko‘k", // blue
+    "green": "yashil", // green
+    "yellow": "sariq", // yellow
+    "orange": "apelsin rangi", // orange
+    "pink": "pushti", // pink
+    "purple": "binafsha", // purple
+    "black": "qora", // black
+    "white": "oq", // white
+    "gray": "kulrang", // gray
+    "brown": "jigarrang", // brown
+    "gold": "oltin", // gold
+    "silver": "kumush", // silver
+    "bronze": "bronza", // bronze
+    "metal": "metal", // metal
+    "wood": "daraxt", // wood
+    "paper": "qog‘oz", // paper
+    "plastic": "plastik", // plastic
+    "glass": "shisha", // glass
+    "cloth": "matolar", // cloth
+    "stone": "tosh", // stone
+    "ceramic": "keramika", // ceramic
+    "fabric": "to‘qimachilik", // fabric
+    "leather": "teridan", // leather
+    "rubber": "rezina", // rubber
+    "paint": "bo‘yash", // paint
+    "brush": "cho‘tkalar", // brush
+    "shadow": "soyasi", // shadow
+    "light": "yorug‘lik", // light
+    "darkness": "qorong‘ilik", // darkness
+    "rainbow": "kamalak", // rainbow
+    "sun": "quyosh", // sun
+    "moon": "oy", // moon
+    "star": "yulduz", // star
+    "planet": "sayyora", // planet
+    "galaxy": "galaksiya", // galaxy
+    "universe": "koinot", // universe
+    "space": "kosmos", // space
+    "time": "vaqt", // time
+    "clock": "soat", // clock
+    "minute": "daqiqa", // minute
+    "hour": "soat", // hour
+    "second": "soniya", // second
+    "day": "kun", // day
+    "week": "hafta", // week
+    "month": "oy", // month
+    "year": "yil", // year
+    "decade": "o‘n yil", // decade
+    "century": "asr", // century
+    "millennium": "ming yillik", // millennium
+    "calendar": "kalendar", // calendar
+    "schedule": "jadval", // schedule
+    "holiday": "bayram", // holiday
+    "birthday": "tug‘ilgan kun", // birthday
+    "new year": "yangi yil", // new year
+    "festival": "festival", // festival
+    "party": "partiya", // party
+    "event": "voqea", // event
+    "meeting": "uchrashuv", // meeting
+    "conference": "konferensiya", // conference
+    "seminar": "seminar", // seminar
+    "workshop": "seminar", // workshop
+    "presentation": "taqdimot", // presentation
+    "speech": "nutq", // speech
+    "lecture": "lektsiya", // lecture
+    "course": "kurs", // course
+    "training": "ta’lim", // training
+    "exam": "imtihon", // exam
+    "test": "test", // test
+    "certificate": "sertifikat", // certificate
+    "degree": "daraja", // degree
+    "graduate": "bitiruvchi", // graduate
+    "student": "talaba", // student
+    "teacher": "o‘qituvchi", // teacher
+    "principal": "maktab direktori", // principal
+    "hello": "salom" // hello
+};
+
+let translation1 = {
+    "kompyuter": "computer",
+    "klaviatura": "keyboard",
+    "sichqoncha": "mouse",
+    "ekran": "screen",
+    "telefon": "phone",
+    "internet": "internet",
+    "vebsayt": "website",
+    "elektron pochta": "email",
+    "parol": "password",
+    "foydalanuvchi nomi": "username",
+    "dasturiy ta’minot": "software",
+    "qattiq jihoz": "hardware",
+    "bulut": "cloud",
+    "tarmoq": "network",
+    "xavfsizlik": "security",
+    "sun’iy intellekt": "artificial intelligence",
+    "mashina o‘rganishi": "machine learning",
+    "ma’lumotlar bazasi": "database",
+    "server": "server",
+    "mijoz": "client",
+    "old qism": "frontend",
+    "orqa qism": "backend",
+    "framework": "framework",
+    "kutubxona": "library",
+    "ochiq kodli": "open source",
+    "o‘zgaruvchi": "variable",
+    "funksiya": "function",
+    "tsikl": "loop",
+    "shart": "condition",
+    "istisno": "exception",
+    "fayl": "file",
+    "katalog": "directory",
+    "bajarish": "execute",
+    "kompilyatsiya qilish": "compile",
+    "sintaksis": "syntax",
+    "xato": "error",
+    "samaradorlik": "performance",
+    "optimallashtirish": "optimization",
+    "joylashtirish": "deployment",
+    "algoritm": "algorithm",
+    "ma’lumot": "data",
+    "obyekt": "object",
+    "klass": "class",
+    "meros olish": "inheritance",
+    "polimorfizm": "polymorphism",
+    "inkapsulyatsiya": "encapsulation",
+    "abstraksiya": "abstraction",
+    "interfeys": "interface",
+    "modul": "module",
+    "paket": "package",
+    "repositoriy": "repository",
+    "versiya nazorati": "version control",
+    "commit": "commit",
+    "shox": "branch",
+    "birlashirish": "merge",
+    "jo‘natish": "push",
+    "olish": "pull",
+    "klonlash": "clone",
+    "bazasi": "database",
+    "jadval": "table",
+    "ustun": "column",
+    "qator": "row",
+    "so‘rov": "query",
+    "qo‘shish": "insert",
+    "yangilash": "update",
+    "o‘chirish": "delete",
+    "tanlash": "select",
+    "birlashish": "join",
+    "indeks": "index",
+    "transaksiya": "transaction",
+    "zaxira nusxasi": "backup",
+    "tiklash": "restore",
+    "shifrlash": "encryption",
+    "autentifikatsiya": "authentication",
+    "avtorizatsiya": "authorization",
+    "sessiya": "session",
+    "cookie": "cookie",
+    "keshlash": "cache",
+    "API": "API",
+    "endpoint": "endpoint",
+    "so‘rov": "request",
+    "javob": "response",
+    "protokol": "protocol",
+    "HTTP": "HTTP",
+    "HTTPS": "HTTPS",
+    "TCP": "TCP",
+    "UDP": "UDP",
+    "DNS": "DNS",
+    "IP manzil": "IP address",
+    "port": "port",
+    "router": "router",
+    "firewall": "firewall",
+    "kiberxavfsizlik": "cybersecurity",
+    "hayvon": "animal",
+    "mushuk": "cat",
+    "it": "dog",
+    "qush": "bird",
+    "baliq": "fish",
+    "ot": "horse",
+    "sigir": "cow",
+    "qo‘y": "sheep",
+    "echki": "goat",
+    "cho‘chqa": "pig",
+    "tovuq": "chicken",
+    "o‘rdak": "duck",
+    "quyon": "rabbit",
+    "yo‘lbars": "lion",
+    "yovvoyi mushuk": "tiger",
+    "fil": "elephant",
+    "jirafa": "giraffe",
+    "zebra": "zebra",
+    "maymun": "monkey",
+    "ayiq": "bear",
+    "bo‘ri": "wolf",
+    "bo‘ri": "fox",
+    "echkikucha": "squirrel",
+    "sichqon": "mouse",
+    "sichqon": "rat",
+    "ilon": "snake",
+    "aligator": "crocodile",
+    "reqa": "shark",
+    "balina": "whale",
+    "karxona": "octopus",
+    "meduza": "jellyfish",
+    "koral": "coral",
+    "daraxt": "tree",
+    "gul": "flower",
+    "o‘simlik": "plant",
+    "varaq": "leaf",
+    "meva": "fruit",
+    "urug‘": "seed",
+    "grunt": "soil",
+    "ildiz": "root",
+    "shox": "branch",
+    "bosh": "trunk",
+    "po‘st": "bark",
+    "o‘rmon": "forest",
+    "cho‘l": "desert",
+    "tog‘": "mountain",
+    "it": "dog",
+    "dolina": "valley",
+    "daryo": "river",
+    "ko‘l": "lake",
+    "okean": "ocean",
+    "deniz": "sea",
+    "orol": "island",
+    "sohil": "beach",
+    "qum": "sand",
+    "to‘p": "stone",
+    "tog‘ tosh": "rock",
+    "bulut": "cloud",
+    "yomg‘ir": "rain",
+    "qor": "snow",
+    "bo‘ron": "storm",
+    "shamol": "wind",
+    "jilg‘a": "thunder",
+    "yilduz": "lightning",
+    "harorat": "temperature",
+    "namlik": "humidity",
+    "zamin silkinishi": "earthquake",
+    "vulkan": "volcano",
+    "tornado": "tornado",
+    "to‘fon": "flood",
+    "tsunami": "tsunami",
+    "olov": "fire",
+    "tutun": "smoke",
+    "portlash": "explosion",
+    "ifloslanish": "pollution",
+    "toza": "clean",
+    "iflos": "dirty",
+    "yorqin": "bright",
+    "qorong‘i": "dark",
+    "issiqlik": "hot",
+    "sovuq": "cold",
+    "iliq": "warm",
+    "salqin": "cool",
+    "nam": "wet",
+    "quruq": "dry",
+    "yumshoq": "soft",
+    "qattiq": "hard",
+    "o‘tkir": "sharp",
+    "sayqallashmagan": "dull",
+    "tez": "fast",
+    "sekin": "slow",
+    "og‘ir": "heavy",
+    "yengil": "light",
+    "katta": "big",
+    "kichik": "small",
+    "baland": "tall",
+    "qisqa": "short",
+    "keng": "wide",
+    "tor": "narrow",
+    "uzun": "long",
+    "chuqur": "deep",
+    "yuzaki": "shallow",
+    "toza": "clean",
+    "iflos": "dirty",
+    "dumaloq": "round",
+    "kvadrat": "square",
+    "uchburchak": "triangle",
+    "to‘rtburchak": "rectangle",
+    "doira": "circle",
+    "rang": "color",
+    "qizil": "red",
+    "ko‘k": "blue",
+    "yashil": "green",
+    "sariq": "yellow",
+    "apelsin rangi": "orange",
+    "pushti": "pink",
+    "binafsha": "purple",
+    "qora": "black",
+    "oq": "white",
+    "kulrang": "gray",
+    "jigarrang": "brown",
+    "oltin": "gold",
+    "kumush": "silver",
+    "bronza": "bronze",
+    "metal": "metal",
+    "daraxt": "wood",
+    "qog‘oz": "paper",
+    "plastik": "plastic",
+    "shisha": "glass",
+    "matolar": "cloth",
+    "tosh": "stone",
+    "keramika": "ceramic",
+    "to‘qimachilik": "fabric",
+    "teridan": "leather",
+    "rezina": "rubber",
+    "bo‘yash": "paint",
+    "cho‘tkalar": "brush",
+    "soyasi": "shadow",
+    "yorug‘lik": "light",
+    "qorong‘ilik": "darkness",
+    "kamalak": "rainbow",
+    "quyosh": "sun",
+    "oy": "moon",
+    "yulduz": "star",
+    "sayyora": "planet",
+    "galaksiya": "galaxy",
+    "koinot": "universe",
+    "kosmos": "space",
+    "vaqt": "time",
+    "soat": "clock",
+    "daqiqa": "minute",
+    "soat": "hour",
+    "soniya": "second",
+    "kun": "day",
+    "hafta": "week",
+    "oy": "month",
+    "yil": "year",
+    "o‘n yil": "decade",
+    "asr": "century",
+    "ming yillik": "millennium",
+   
+}
+
+
+class Translation {
+    constructor() {
+        this.input = document.querySelector("#input-text");
+        this.selected = null;  
+        this.result = document.querySelector(".result");
+        this.p = document.querySelector("#translated-text");
+        this.button = document.querySelector("#translate-btn");
+        this.fromLang = document.querySelector("#from-lang");
+        this.toLang = document.querySelector("#to-lang");  
+        this.addEventListener();
+    }
+
+    addEventListener() {
+        this.button.addEventListener("click", () => {
+            this.trim();
+        });
+    }
+
+    trim() {
+        const fromLangValue = this.fromLang.value;
+        const toLangValue = this.toLang.value;
+
+        const inputValue = this.input.value.trim().toLowerCase();
+        
+        if (fromLangValue === 'en' && toLangValue === 'uz') {  
+            if (translation[inputValue]) {
+                this.selected = translation[inputValue];
+                this.p.innerHTML = this.selected;
+                this.result.innerHTML = ''; 
+                this.result.appendChild(this.p);
+            } else {
+                this.p.innerHTML = "No translation found"; 
+                this.result.innerHTML = '';
+                this.result.appendChild(this.p);
+            }
+        } else if (fromLangValue === 'uz' && toLangValue === 'en') {
+            if (translation1[inputValue]) {
+                this.selected = translation1[inputValue];
+                this.p.innerHTML = this.selected;
+                this.result.innerHTML = ''; 
+                this.result.appendChild(this.p);
+            } else {
+                this.p.innerHTML = "No translation found"; 
+                this.result.innerHTML = '';
+                this.result.appendChild(this.p);
+            }
+        } else {
+            this.p.innerHTML = "Translation not available for selected languages";
+            this.result.innerHTML = '';
+            this.result.appendChild(this.p);
+        }
+    }
+}
+
+const tre = new Translation();
+
